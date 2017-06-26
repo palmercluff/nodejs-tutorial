b7f35b2551ff522500d344840b54268ca37ddc4f
Palmer Cluff <palmercluff@gmail.com>
2017-06-26 22:07:17 +0000

    HTTP module examples

---
+++ first_server_split_query_string.js
@@ -0,0 +1,10 @@
+// http://localhost:8080/?year=2017&month=July will produce "2017 July"
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
+}).listen(8080);
